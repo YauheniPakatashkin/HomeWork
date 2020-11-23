c4bd230e782ffc3b4bb9421cb579c1463d402f54
Yauheni <stinkor06@mail.ru>
2020-11-24 01:43:42 +0300

    HW2

--- src/homeWork2/lesson2.java
+++ src/homeWork2/lesson2.java
@@ -31,6 +31,25 @@ public class lesson2 {
         System.out.println(rezultat(6, 6));//метод описан ниже
 
         //Задача 5
+        int duim = 0;
+        String w = null;
+        String v = null;
+        for (int i = 1; i < 21; i++) {
+            duim=+i;
+            if (duim==1){ w = "дюйм"; v = "равен";}
+            if (duim>1 && duim<5){ w = "дюйма"; v = "равны";}
+            if (duim>4) { w = "дюймов"; v = "равны";}
+            System.out.println(duim+" "+w+" "+v+" "+perevodDuimVsantimetr(duim)+" см");
+        }
+
+        //Задача 6
+        int p =2;
+        for (; p <101 ; p++) {
+            if (p % 2 == 0) {
+                System.out.print(p+" ");
+            }
+        }
+
 
     }
 
@@ -40,5 +59,9 @@ public class lesson2 {
             c +=a;
         }
         return c;
+    }
+    public static double perevodDuimVsantimetr(int duim) { //метод к задаче 5, пеервод дюймов в сантиметры
+        double sm = duim*2.54;
+        return sm;
     }
 }
