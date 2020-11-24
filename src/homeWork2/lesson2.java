2b77ed0ec5cb032b7528e633488fb13d36032907
Yauheni <stinkor06@mail.ru>
2020-11-24 23:45:37 +0300

    добил 8

--- src/homeWork2/lesson2.java
+++ src/homeWork2/lesson2.java
@@ -35,38 +35,62 @@ public class lesson2 {
         String w = null;
         String v = null;
         for (int i = 1; i < 21; i++) {
-            duim=+i;
-            if (duim==1){ w = "дюйм"; v = "равен";}
-            if (duim>1 && duim<5){ w = "дюйма"; v = "равны";}
-            if (duim>4) { w = "дюймов"; v = "равны";}
-            System.out.println(duim+" "+w+" "+v+" "+perevodDuimVsantimetr(duim)+" см");
+            duim = +i;
+            if (duim == 1) {
+                w = "дюйм";
+                v = "равен";
+            }
+            if (duim > 1 && duim < 5) {
+                w = "дюйма";
+                v = "равны";
+            }
+            if (duim > 4) {
+                w = "дюймов";
+                v = "равны";
+            }
+            System.out.println(duim + " " + w + " " + v + " " + perevodDuimVsantimetr(duim) + " см");
         }
 
         //Задача 6
-        int p =2;
-        for (; p <101 ; p++) {
+        int p = 2;
+        for (; p < 101; p++) {
             if (p % 2 == 0) {
-                System.out.print(p+" ");
+                System.out.print(p + " ");
             }
         }
         System.out.println("");
         //Задача 7
         int count1 = 0;
-        for (int i = 0; i <100; i++) {
-            if(i%2!=0)
-                count1+=i;
+        for (int i = 0; i < 100; i++) {
+            if (i % 2 != 0)
+                count1 += i;
         }
 
-        System.out.println("Сумма нечетных чисел от 1 до 99 равна "+ count1);
+        System.out.println("Сумма нечетных чисел от 1 до 99 равна " + count1);
 
         //Задача 8 в процессе
         for (int i = 0; i < 5; i++) {
-            System.out.println();
-            for (int j = 0; j < i; j++) {
-                System.out.print("*");
+            for (int j = 4; j > 0; j--) {
+                if (i < j)
+                    System.out.print(" ");
+                else
+                    System.out.print("*");
             }
+            System.out.println();
 
         }
+        System.out.println(" ");
+        for (int i = 4; i >0; i--) {
+            for (int j = 4; j > 0; j--) {
+                if (i < j)
+                    System.out.print(" ");
+                else
+                    System.out.print("*");
+            }
+            System.out.println();
+
+        }
+
 
 
     }
@@ -74,12 +98,13 @@ public class lesson2 {
     public static int rezultat(int a, int b) { //метод к задаче 4, умножение заменяем сложением
         int c = 0;
         for (int i = 0; i < b; i++) {
-            c +=a;
+            c += a;
         }
         return c;
     }
+
     public static double perevodDuimVsantimetr(int duim) { //метод к задаче 5, пеервод дюймов в сантиметры
-        double sm = duim*2.54;
+        double sm = duim * 2.54;
         return sm;
     }
 }
