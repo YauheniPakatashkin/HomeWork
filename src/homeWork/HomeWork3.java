913d2328a0f18855802a6c0d868180db400b8974
Yauheni <stinkor06@mail.ru>
2020-12-01 21:44:40 +0300

    hw3

--- src/homeWork/HomeWork3.java
+++ src/homeWork/HomeWork3.java
@@ -50,13 +50,14 @@ public class HomeWork3 {
             System.out.print(array1[i]);
             System.out.print(" ");
         }
+        System.out.println(" ");
     }
 
     public static void task3() {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
         int even = 0;
-        System.out.println("Введите длину массива");
+        System.out.println("Введите длину массива 15");
         int arrayLength = scanner.nextInt();
         int[] array2 = new int[arrayLength];
         for (int i = 0; i < array2.length; i++) {
@@ -67,13 +68,14 @@ public class HomeWork3 {
         }
         System.out.println(Arrays.toString(array2));
         System.out.println("Количество чётных чисел в массиве " + even);
+        System.out.println(" ");
     }
 
     public static void task4() {
         int[] array3 = new int[20];
         Random random = new Random();
         for (int i = 0; i < array3.length; i++) {
-            array3[i] = random.nextInt(20);
+            array3[i] = random.nextInt(21);
         }
         System.out.println(Arrays.toString(array3));
         for (int i = 0; i < array3.length; i++) {
@@ -82,6 +84,7 @@ public class HomeWork3 {
             }
         }
         System.out.println(Arrays.toString(array3));
+        System.out.println(" ");
     }
 
     public static void task5() {
@@ -90,8 +93,8 @@ public class HomeWork3 {
         int[] array1 = new int[5];
         Random random = new Random();
         for (int i = 0; i < 5; i++) {
-            array[i] = random.nextInt(15);
-            array1[i] = random.nextInt(15);
+            array[i] = random.nextInt(16);
+            array1[i] = random.nextInt(16);
         }
         System.out.println(Arrays.toString(array));
         System.out.println(Arrays.toString(array1));
@@ -100,10 +103,10 @@ public class HomeWork3 {
             a += array[i];
             b += array1[i];
         }
-        System.out.println(a);
-        System.out.println(b);
         double c = a / 5;
         double d = b / 5;
+        System.out.println("Среднее арифметическое первого массива "+c);
+        System.out.println("Среднее арифметическое первого массива "+d);
         if (c > d) {
             System.out.println("Среднее арифметическое первого массива больше");
         }
@@ -113,30 +116,30 @@ public class HomeWork3 {
         if (c == d) {
             System.out.println("Средние арифметические массивов равны");
         }
+        System.out.println(" ");
+
     }
 
     public static void task6() {
-        while (true) {
-            int[] array = new int[4];
-            Random random = new Random();
-            for (int i = 0; i < 3; i++) {
-                array[i] = random.nextInt(10);
-            }
-            int count = 0;
-            System.out.println(Arrays.toString(array));
-            for (int i = 0; i < array.length - 1; i++) {
-                if (array[i] < array[i + 1]) {
-                    count++;
-                }
-                System.out.println(count);
-
-            }
-            if (count == array.length - 1) {
-                System.out.println("является");
-                break;
-            } else
-                System.out.println("не является");
+        int[] array = new int[4];
+        Random random = new Random();
+        for (int i = 0; i < 3; i++) {
+            array[i] = random.nextInt(11);
         }
+        int count = 0;
+        System.out.println(Arrays.toString(array));
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] < array[i + 1]) {
+                count++;
+            }
+        }
+        if (count == array.length - 1) {
+            System.out.println("является");
+        } else {
+            System.out.println("не является");
+        }
+        System.out.println(" ");
+
     }
 
     public static void task7() {
@@ -152,7 +155,7 @@ public class HomeWork3 {
                 max = array2[i];
             }
         }
-        System.out.println(max);
+        System.out.println("Максимальное число "+max);
 
 
     }
