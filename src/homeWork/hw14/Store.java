96930d255814539513a29ebac591cc3390ce5f2d
Yauheni <stinkor06@mail.ru>
2021-02-04 21:15:10 +0300

    Home work 14

--- src/homeWork/hw14/Store.java
+++ src/homeWork/hw14/Store.java
@@ -3,5 +3,13 @@ package homeWork.hw14;
 import java.util.ArrayList;
 
 public class Store {
+    public static void main(String[] args) {
+
+    }
+
     ArrayList<String>productList = new ArrayList<>();
+    public  static void addProduct(){
+
+    }
+
 }
