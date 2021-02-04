9d8b27d39efc7a85bab1c04d1693244ad13a12ad
Yauheni <stinkor06@mail.ru>
2021-02-04 21:08:42 +0300

    Home work 14

---
+++ src/homeWork/hw14/Product.java
@@ -0,0 +1,7 @@
+package homeWork.hw14;
+
+public class Product {
+    private int id;
+    private String name;
+    private int price;
+}
---
+++ src/homeWork/hw14/Store.java
@@ -0,0 +1,7 @@
+package homeWork.hw14;
+
+import java.util.ArrayList;
+
+public class Store {
+    ArrayList<String>productList = new ArrayList<>();
+}
